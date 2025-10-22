945741d41135aa2d8d7183111aad4e1794558cfa
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-22 02:55:53 -0700

    update hero sec

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-
+import  React from 'react'
+import { useState } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { heroVideo, smallHeroVideo} from '../utils'
 const Hero = () => {
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 768 ? smallHeroVideo : heroVideo)
+
+    useGSAP(() =>{
+        gsap.to('#hero',{opacity:1 ,delay:1.5 })
+    },[])
   return (
-    <div>
-      
-    </div>
+    <section className='w-full nav-height bg-black relative'>
+       <div className='h-5/6 w-full flex-center flex-col  '>
+         <p id="hero"className='hero-title'>iPhone 15 pro</p>
+         <div className='md:w-10/12 w-9/12'>
+             <video autoPlay muted playsInline={true} key={videoSrc} className='w-full h-full'>
+                <source src={videoSrc} type="video/mp4"/>
+             </video>
+         </div>
+       </div>
+       
+    </section>
   )
 }
 
