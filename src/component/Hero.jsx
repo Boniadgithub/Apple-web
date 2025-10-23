d91936a08da43bc7c19b9b13e4cfefe6c453e6a2
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-23 02:49:45 -0700

    highlight title added

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -21,13 +21,7 @@ const Hero = () => {
     }
     },[])
     
-      
-
-
-
-
-
-    useGSAP(() =>{
+      useGSAP(() =>{
         gsap.to('#hero',{opacity:1 ,delay:1.5 })
         gsap.to('#cta',{opacity:1 ,y:-50 ,delay:2})
     },[])
--- src/component/Highlights.jsx
+++ src/component/Highlights.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import React, { use } from 'react' 
+import gsap from 'gsap' 
 
 const Highlights = () => {
+
+  useGSAP(() =>{
+    gsap.to('#title',{opacity:1,y:0},)
+  },[])
   return (
-    <div>
-    
-    </div>
+    <section id='Highlights' className="w-screen overflow-hidden h-full common-padding bg-zinc">
+      <div className='w-full items-end justify-between mb-20'>
+        <h2 className='section-heading' id="title">Get the Highlights</h2>
+        </div>
+    </section>
   )
 }
 
