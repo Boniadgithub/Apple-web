aa96781e496b1281d2b7a68ffc4b642e5c011130
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-23 04:53:00 -0700

    update in video play

--- src/component/VideoCarousel.jsx
+++ src/component/VideoCarousel.jsx
@@ -1,6 +1,51 @@
-import React from 'react'
+import React, { use, useEffect } from 'react'
 import  { hightlightsSlides } from '../constants'
+import { useRef, useState } from 'react'
 const VideoCarousel = () => {
+    const videoRef = useRef([]);
+    const videoSpanRef = useRef([]);
+    const videoDivRef = useRef([]);
+    const [video ,setVideo] = useState({
+        isEnd:false,
+        
+        startPlay:false,
+        videoId:0,
+        isLastVideo:false, 
+        
+        isPlaying:false,
+    })
+    const [loadedData, setLoadedData] = useState([]);
+    const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
+    useEffect(()=>{
+        if(loadedData.length > 3){
+            if (!isPlaying){
+                videoRef.current[videoId].pause();
+            }
+            else{
+                startPlay&& videoRef.current[videoId].play();
+            }
+        }
+
+    },[startPlay, videoId, isPlaying, loadedData])
+     
+    useEffect (()=>{
+        const currentProress =0;
+        let span =videoSpanRef.current;
+        let videoDiv =videoDivRef.current;
+        let videoElement =videoRef.current;
+        if (span[videoId]){
+           let anim = gsap.to(span[videoId],{
+            onUpdate:()=>{
+
+            },
+            onComplete:()=>{}
+
+           })
+        }
+    })
+    
+
+    
   return (
     <>
       <div className='flex items-center'>
@@ -13,6 +58,12 @@ const VideoCarousel = () => {
                   <video
                      id='video' playsInline={true}
                      preload='auto'
+                     ref={(el) => (videoRef.current[i] = el)}
+                     onPlay={()=>{
+                        setVideo((prevVideo)=>({
+                            ...prevVideo,
+                            isPlaying:true,}))
+                     }}
                   >
                     <source src={list.video} type="video/mp4"/>
                   </video>
@@ -29,6 +80,24 @@ const VideoCarousel = () => {
                 </div>
         ))}
       </div>
+        <div className='relative flex-center mt-10'>
+<div className='flex-center py-5 px-7 bg-gray-300 backdrop-blur
+ rounded-full'>
+    {videoRef.current.map((_,i)=>(
+        <span key={i} ref={(el)=>(videoDivRef.current[i]=
+            el
+        )} className='mx-2 w-3 h-3 bg-gray-200 rounded-full
+        relative cursor-pointer'>
+            <span className='absolute h-full w-full rounded-full '>
+
+            </span>
+
+        </span>
+    )
+    )}
+
+</div>
+        </div>
     </>
   )
 }
