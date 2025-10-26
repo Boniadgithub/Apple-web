c1758e6d1f96e71fcbb4ee6800eeb441828668b7
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-10-26 02:18:51 -0700

    animate with gsap added

--- src/utils/animations.jsx
+++ src/utils/animations.jsx
@@ -1,10 +1,42 @@
-// src/utils/animations.jsx
-import gsap from "gsap";
+import gsap from "gsap"
 
-export const animateWithGsapTimeline = (timeline, target, vars) => {
-  timeline.to(target, vars);
-};
+import { ScrollTrigger } from "gsap/all"
+gsap.registerPlugin(ScrollTrigger);
 
-export const animateWithGsap = (target, vars) => {
-  gsap.to(target, vars);
-};
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: 'restart reverse restart reverse',
+      start: 'top 85%',
+      ...scrollProps,
+    }
+  })
+}
+
+export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
+  timeline.to(rotationRef.current.rotation, {
+    y: rotationState,
+    duration: 1,
+    ease: 'power2.inOut'
+  })
+
+  timeline.to(
+    firstTarget,
+    {
+      ...animationProps,
+      ease: 'power2.inOut'
+    },
+    '<'
+  )
+
+  timeline.to(
+    secondTarget,
+    {
+      ...animationProps,
+      ease: 'power2.inOut'
+    },
+    '<'
+  )
+}
